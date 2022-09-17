60805912ce6a33e1906babc19fe90533a02741fb
charter <charter43@gmail.com>
2022-09-17 20:58:07 +0700

    Tantangan: Promise
    mengerjakan tantangan promise

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const {
+  promiseTheaterIXX,
+  promiseTheaterVGC
+} = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (params) => {
+  let theaterIXX = await promiseTheaterIXX();
+  let theaterVGC = await promiseTheaterVGC();
+
+  return new Promise((resolve) => {
+    const hasil = [...theaterIXX, ...theaterVGC] ?
+      .filter(res => res.hasil === params) ? .length
+    return resolve(hasil);
+  });
+}
+
 
 module.exports = {
   promiseOutput,
